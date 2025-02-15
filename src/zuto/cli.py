f74e57582d74805ab83ed7a031e927e81fc3783d
ZackaryW <gitzackw@gmail.com>
2025-02-14 21:25:14 -0800

    feat: init

---
+++ src/zuto/cli.py
@@ -0,0 +1,16 @@
+import click
+from zuto import Zuto
+import os
+
+@click.command()
+@click.option("-p", "--path", type=click.Path(exists=True), help="Path to the tasks folder")
+def cli(path):
+    if not path:
+        path = os.getcwd()
+
+    zuto = Zuto(path)
+    zuto.start()
+
+if __name__ == "__main__":
+    cli()
+
